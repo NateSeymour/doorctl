eaef403bed865bf7d060047e987cdadee46b3486
Nathan Seymour <nathan@seymour.global>
2025-12-18 14:13:48 -0500

    feat(api/access): implement key signing with Amazon KMS

--- api/access/index.ts
+++ api/access/index.ts
@@ -1,8 +1,38 @@
+import {
+  KMSClient,
+  SignCommand,
+} from '@aws-sdk/client-kms';
+
 export const handler = async () => {
+  const client = new KMSClient();
+
+  const keyName = process.env.SIGN_KEY_ALIAS;
+  if (!keyName) {
+    return {
+      statusCode: 500,
+      message: 'No key name is configured for signing.',
+    };
+  }
+
+  const command = `unlock@${Date.now()}`;
+  const { Signature: signature } = await client.send(new SignCommand({
+    KeyId: `alias/${keyName}`,
+    Message: Buffer.from(command, 'ascii'),
+    SigningAlgorithm: 'ECDSA_SHA_256',
+  }));
+
+  if (!signature) {
+    return {
+      statusCode: 500,
+      message: 'Failed to sign command.',
+    };
+  }
+
   return {
     statusCode: 200,
     body: JSON.stringify({
-      version: 'v0.0.1',
+      command,
+      signature: Buffer.from(signature).toString('base64'),
     }),
   };
 };
